24c3c1ceeb9898434212d46b20224e708e3c016e
= <=>
2021-07-18 19:42:46 -0300

    Inicio do projeto, DTO e Spring Data JPA

---
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -0,0 +1,16 @@
+package br.com.alura.forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication
+//@EntityScan("com.alura.forum.modelo")
+public class ForumApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ForumApplication.class, args);
+	}
+
+}
